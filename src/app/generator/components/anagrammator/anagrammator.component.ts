bc42bdc8a07e75c4db3e4fa1c421bffdb526c23d
Rost Siryk <rostislav.siryk@gmail.com>
2022-09-03 15:57:47 +0300

    get axes - wip

--- src/app/generator/components/anagrammator/anagrammator.component.ts
+++ src/app/generator/components/anagrammator/anagrammator.component.ts
@@ -120,7 +120,7 @@ export class AnagrammatorComponent implements OnInit {
   axes: GameAxis[] =
     [
       {
-        name: 'Humanity',
+        name: 'Модерн',
         values: [
           { label: 'Свобода' },
           { label: 'Братерство' },
@@ -128,7 +128,7 @@ export class AnagrammatorComponent implements OnInit {
         ],
       },
       {
-        name: 'Humanity',
+        name: 'Архаїка',
         values: [
           { label: 'Війна' },
           { label: 'Релігія' },
@@ -136,7 +136,7 @@ export class AnagrammatorComponent implements OnInit {
         ]
       },
       {
-        name: 'Humanity',
+        name: 'Діло',
         values: [
           { label: 'Наука' },
           { label: 'Містика' },
@@ -144,7 +144,7 @@ export class AnagrammatorComponent implements OnInit {
         ]
       },
       {
-        name: 'Humanity',
+        name: 'Ідея',
         values:
           [
             { label: 'Ієрархія' },
@@ -152,30 +152,124 @@ export class AnagrammatorComponent implements OnInit {
             { label: 'Хаос' },
           ]
       }
-    ]
+    ];
 
+  getValues(axisValues: GameValue[] ) {
+    if (axisValues.length === 0) return [{label: ''}];
+    let result = {} as any;
+    axisValues.forEach((val, i) => {
+      let remainingItems = axisValues.slice(0, i).concat(axisValues.slice(i + 1));
+      this.getValues(remainingItems).forEach((remaining) => {
+        const res = [val].concat(remaining);
+        const id = val.label + remainingItems.map(ri => ri.label)
+        result[id] = res;
+      });
+    })
+    return Object.values(result) as [];
+  }
+
+  // if (items.length === 0) return [''];
+  // var result = {} as any;
+  // items.forEach((item, i) => {
+  //   var remainingItems = items.slice(0, i).concat(items.slice(i + 1));
+  //   this.combineItems(remainingItems, max, start).forEach((remaining = '') => {
+  //     const res = [item].concat(remaining);
+  //     const id = item + remaining
+  //     result[id] = res;
+  //   });
+  // });
+  // return Object.values(result) as [];
+
+  getCombos(){
+    for(let a = 0; a < this.axes.length; a++) {
+      const axeValues = this.axes[a].values;
+      console.log(axeValues.map(v => v.label));
+      const an = this.getValues(this.axes[a].values);
+      console.log('an:', an);
+    }
+  }
+  
   triangles: string[] = [];
 
-  t1 = [''];
-  t2 = [''];
-  t3 = [''];
-  t4 = [''];
-  bookConfig = [''];
+  tModern = [''];
+  tArchaic = [''];
+  tCraft = [''];
+  tIdea = [''];
+  bookConfigs: any[];
+
+  // combineAxes(axis: GameAxis, max = this.maxIterations, start = 0, divider = '') {
+  //   const axeValues = axis.values;
+  //   if (axeValues.length === 0) return [''];
+  //   var result = {} as any;
+  //   axeValues.forEach((axeValue, i) => {
+  //     var remainingItems = axeValues.slice(0, i).concat(axeValues.slice(i + 1));
+  //     this.combineItems(remainingItems, max, start).forEach((remaining = '') => {
+  //       const res = [axeValue].concat(remaining);
+  //       const id = axeValue + remaining
+  //       result[id] = res;
+  //     });
+  //   });
+  //   return Object.values(result) as [];
+  // }
+
+  senses: string[][] = [];
+
+  getSenses2() {
+    for (let i = 0; i < 6; i+=2) {
+      const categoryModern = this.tModern[i][0];
+      const categoryArchaic = this.tArchaic[i][0];
+      const categoryCraft = this.tCraft[i][0];
+      const categoryIdea = this.tIdea[i][0];
+      this.senses.push([categoryModern, categoryArchaic, categoryCraft, categoryIdea])
+    }
+  }
+
+  parsedConfigs: any[];
+
+  parseBookConfigs() {
+    const configsParsed: any[] = [];
+    const configsParsed2: any[] = [];
+    this.bookConfigs.forEach(bc => {
+      const bookAxes = bc as [];
+      configsParsed2.push([bc[0][0][0], bc[1][0][0], bc[2][0][0], bc[3][0][0]] );
+
+      bookAxes.forEach(axis => {
+        const axeValues = axis as [];
+        // configsParsed2.push([axis[0][0][0], axis[1][0][0], axis[2][0][0], axis[3][0][0]] );
+        
+        if (typeof axeValues === 'object') {
+          console.log('av:', axeValues);
+          axeValues.forEach(value => {
+                configsParsed.push(value[0])
+          })
+        }
+        
+      })
+    })
+
+    this.parsedConfigs = configsParsed2;
+    
+    return configsParsed as any;
+  }
 
   getAxes() {
-    this.t1 = this.combineItems(this.axes1[0], undefined, undefined, ', ');
-    this.t2 = this.combineItems(this.axes1[1], undefined, undefined, ', ');
-    this.t3 = this.combineItems(this.axes1[2], undefined, undefined, ', ');
-    this.t4 = this.combineItems(this.axes1[3], undefined, undefined, ', ');
-    this.bookConfig = this.combineItems([this.t1, this.t2, this.t3, this.t4], undefined, undefined, ', ');
+    this.tModern = this.combineItems(this.axes1[0], undefined, undefined, ', ');
+    this.tArchaic = this.combineItems(this.axes1[1], undefined, undefined, ', ');
+    this.tCraft = this.combineItems(this.axes1[2], undefined, undefined, ', ');
+    this.tIdea = this.combineItems(this.axes1[3], undefined, undefined, ', ');
 
-    // console.log(this.t1.join('\n'));
-    // console.log(this.t2.join('\n'));
-    // console.log(this.t3.join('\n'));
-    // console.log(this.t4.join('\n'));
-    // console.log(this.bookConfig.join('\n'));
-    console.log(this.bookConfig);
+    this.bookConfigs = this.combineItems([this.tModern, this.tArchaic, this.tCraft, this.tIdea], undefined, undefined, ', ');
 
+    console.log('TMOD:', this.tModern);
+
+    console.log('BCS:', this.bookConfigs);
+
+    this.getSenses2();
+
+    // this.parseBookConfigs();
+
+    this.getCombos();
+    
   }
 
 }
