ed56542c27beb461e657b138cf16cc9c1495bcda
Rost Siryk <rostislav.siryk@gmail.com>
2022-09-25 01:02:26 +0300

    feat: format axes and values, add description

--- src/app/generator/components/model-axes/model-axes.component.ts
+++ src/app/generator/components/model-axes/model-axes.component.ts
@@ -10,6 +10,11 @@ export class ModelAxesComponent implements OnInit {
 
   combinations = [''];
 
+  // axesAsStrings: string[][] = [
+  //   ['Свобода', 'Братерство'],
+  //   ['Війна', 'Релігія'],
+  // ]
+
   axesAsStrings: string[][] = [
     ['Свобода', 'Братерство', 'Рівність'],
     ['Війна', 'Релігія', 'Секс'],
@@ -19,6 +24,7 @@ export class ModelAxesComponent implements OnInit {
 
   ngOnInit() {
     this.getCombinations(0, this.axesAsStrings, '');
+    this.combinations.shift();
   }
 
   getCombinations(pos: number, c: string[][], soFar: string) {
@@ -27,7 +33,7 @@ export class ModelAxesComponent implements OnInit {
       return;
     }
     for (let i = 0; i < c[pos].length; i++) {
-      this.getCombinations(pos + 1, c, soFar + ' - ' + c[pos][i]);
+      this.getCombinations(pos + 1, c, soFar + c[pos][i] + '. ');
     }
   }
 }
