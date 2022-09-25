81d278e1ecb098ad8062ceffb70c13a433685e10
Rost Siryk <rostislav.siryk@gmail.com>
2022-09-26 00:39:44 +0300

    Init pages build
    update base href
    
    rebuild
    
    rebuild
    
    rebuild
    
    update base href
    
    Bump eventsource from 1.1.0 to 1.1.2
    
    Bumps [eventsource](https://github.com/EventSource/eventsource) from 1.1.0 to 1.1.2.
    - [Release notes](https://github.com/EventSource/eventsource/releases)
    - [Changelog](https://github.com/EventSource/eventsource/blob/master/HISTORY.md)
    - [Commits](https://github.com/EventSource/eventsource/compare/v1.1.0...v1.1.2)
    
    ---
    updated-dependencies:
    - dependency-name: eventsource
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump url-parse from 1.5.1 to 1.5.10
    
    Bumps [url-parse](https://github.com/unshiftio/url-parse) from 1.5.1 to 1.5.10.
    - [Release notes](https://github.com/unshiftio/url-parse/releases)
    - [Commits](https://github.com/unshiftio/url-parse/compare/1.5.1...1.5.10)
    
    ---
    updated-dependencies:
    - dependency-name: url-parse
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
