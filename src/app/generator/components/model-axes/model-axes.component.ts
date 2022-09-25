96cd9907a0247e21c62f6f224fa342699975b179
Rost Siryk <rostislav.siryk@gmail.com>
2022-09-25 20:35:08 +0300

    feat(story-values): combine objects, not strings

--- src/app/generator/components/model-axes/model-axes.component.ts
+++ src/app/generator/components/model-axes/model-axes.component.ts
@@ -1,13 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
-type IStrValue = string;
-type IStrValues = IStrValue[];
-type IStrValuesCollection = IStrValues[];
-
-type INumValue = number;
-type INumValues = INumValue[];
-type INumValuesCollection = INumValues[];
-
 type iObj = {
   label: string;
   class: string;
@@ -24,45 +16,33 @@ type IObjValuesCollection = IObjValues[];
   styleUrls: ['./model-axes.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
+
 export class ModelAxesComponent implements OnInit {
 
-  combinations: IStrValues = [''];
+  combinations: IObjValuesCollection = [];
 
   showIndex = false;
 
-  axesAsStrings: IStrValuesCollection = [
-    ['Свобода', 'Братерство', 'Рівність'],
-    ['Війна', 'Релігія', 'Секс'],
-    ['Наука', 'Містика', 'Ремесло'],
-    ['Ієрархія', 'Гармонія', 'Хаос']
-  ]
-
-  axesAsNumbers: INumValuesCollection = [
-    [0, 1],
-    [0, 1],
-  ]
-
-  axesAObjects: IObjValuesCollection = [
-    [{ label: 'Свобода', class: 'axe-1 val-1' }, { label: 'Братерство', class: 'axe-1 val-2' }],
-    [{ label: 'Війна', class: 'axe-2 val-1' }, { label: 'Релігія', class: 'axe-2 val-2' }],
+  axes: IObjValuesCollection = [
+    [{ label: 'Свобода', class: 'axe-1 val-1' }, { label: 'Братерство', class: 'axe-1 val-2' }, { label: 'Рівність', class: 'axe-1 val-3' }],
+    [{ label: 'Війна', class: 'axe-2 val-1' }, { label: 'Релігія', class: 'axe-2 val-2' }, { label: 'Секс', class: 'axe-2 val-3' }],
+    [{ label: 'Наука', class: 'axe-3 val-1' }, { label: 'Містика', class: 'axe-3 val-2' }, { label: 'Ремесло', class: 'axe-3 val-3' }],
+    [{ label: 'Ієрархія', class: 'axe-4 val-1' }, { label: 'Гармонія', class: 'axe-4 val-2' }, { label: 'Хаос', class: 'axe-4 val-3' }],
   ]
 
   ngOnInit() {
-    this.combinations = this.getCombinations(0, [''], this.axesAObjects, '');
-    this.combinations.shift();
+    this.combinations = this.getCombinations(0, [], this.axes, []);
   }
 
-  getCombinations(pos: number, combinations: string[], obj: IObjValuesCollection, soFar: string): string[] {
-    if (pos === obj.length) {
+  getCombinations(pos: number, combinations: IObjValuesCollection, values: IObjValuesCollection, soFar: IObjValues): IObjValuesCollection {
+    if (pos === values.length) {
       combinations.push(soFar);
       return combinations;
     }
-    for (let i = 0; i < obj[pos].length; i++) {
-      const val = obj[pos][i];
-      this.getCombinations(pos + 1, combinations, obj, soFar + this.formatOutput(i, val));
+    for (let i = 0; i < values[pos].length; i++) {
+      const val = values[pos][i];
+      this.getCombinations(pos + 1, combinations, values, soFar.concat(values[pos][i]));
     }
     return combinations;
   }
-
-  formatOutput = (i: number, val: iObj) => `<span class="${val.class}">${this.showIndex ? i + ': ' : ''}${val.label}.</span>`;
 }
