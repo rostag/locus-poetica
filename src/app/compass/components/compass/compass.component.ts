6d8162ef2575f3135d4e5feb35a6d570e59ecbfa
Rost Siryk <rostislav.siryk@gmail.com>
2022-09-27 05:13:26 +0300

    get permutations in lodash-style

--- src/app/compass/components/compass/compass.component.ts
+++ src/app/compass/components/compass/compass.component.ts
@@ -9,6 +9,6 @@ import { Component } from "@angular/core";
 export class CompassComponent {
 
     constructor () {
-        console.log('Compass:', this);
+        
     }
 }
--- src/app/generator/components/model-axes/model-axes.component.ts
+++ src/app/generator/components/model-axes/model-axes.component.ts
@@ -22,36 +22,37 @@ type IObjValuesCollection = IObjValues[];
 export class ModelAxesComponent implements OnInit {
   
   // axes: IObjValuesCollection = [
-  //   [{ label: 'A', class: 'axe-1 val-1' }, { label: 'E', class: 'axe-1 val-2' }],
-  //   [{ label: 'D', class: 'axe-2 val-1' }, { label: 'L', class: 'axe-2 val-2' }],
+  //   [{ label: 'A', class: 'axe-1 val-1' }, { label: 'B', class: 'axe-1 val-2' }, { label: 'C', class: 'axe-1 val-3' }],
+  //   [{ label: 'D', class: 'axe-2 val-1' }, { label: 'E', class: 'axe-2 val-2' }, { label: 'F', class: 'axe-2 val-3' }],
+  //   [{ label: 'D', class: 'axe-2 val-1' }, { label: 'E', class: 'axe-2 val-2' }, { label: 'F', class: 'axe-2 val-3' }],
   // ]
 
-  axes: IObjValuesCollection = [
-    [{ label: 'Свобода', class: 'axe-1 val-1' }, { label: 'Братерство', class: 'axe-1 val-2' }],
-    [{ label: 'Війна', class: 'axe-2 val-1' }, { label: 'Релігія', class: 'axe-2 val-2' }],
-  ]
+  // axes: IObjValuesCollection = [
+  //   [{ label: 'Свобода', class: 'axe-1 val-1' }, { label: 'Братерство', class: 'axe-1 val-2' }],
+  //   [{ label: 'Війна', class: 'axe-2 val-1' }, { label: 'Релігія', class: 'axe-2 val-2' }],
+  // ]
   
   sets: IObjValuesCollection = [];
 
   priorities: IObjValuesCollection = [];
 
-  // axes: IObjValuesCollection = [
-  //   [{ label: 'Свобода', class: 'axe-1 val-1' }, { label: 'Братерство', class: 'axe-1 val-2' }, { label: 'Рівність', class: 'axe-1 val-3' }],
-  //   [{ label: 'Війна', class: 'axe-2 val-1' }, { label: 'Релігія', class: 'axe-2 val-2' }, { label: 'Секс', class: 'axe-2 val-3' }],
-  //   [{ label: 'Наука', class: 'axe-3 val-1' }, { label: 'Містика', class: 'axe-3 val-2' }, { label: 'Ремесло', class: 'axe-3 val-3' }],
-  //   [{ label: 'Ієрархія', class: 'axe-4 val-1' }, { label: 'Гармонія', class: 'axe-4 val-2' }, { label: 'Хаос', class: 'axe-4 val-3' }],
-  // ]
+  axes: IObjValuesCollection = [
+    [{ label: 'Свобода', class: 'axe-1 val-1' }, { label: 'Братерство', class: 'axe-1 val-2' }, { label: 'Рівність', class: 'axe-1 val-3' }],
+    [{ label: 'Війна', class: 'axe-2 val-1' }, { label: 'Релігія', class: 'axe-2 val-2' }, { label: 'Секс', class: 'axe-2 val-3' }],
+    [{ label: 'Наука', class: 'axe-3 val-1' }, { label: 'Містика', class: 'axe-3 val-2' }, { label: 'Ремесло', class: 'axe-3 val-3' }],
+    [{ label: 'Ієрархія', class: 'axe-4 val-1' }, { label: 'Гармонія', class: 'axe-4 val-2' }, { label: 'Хаос', class: 'axe-4 val-3' }],
+  ]
 
   showIndex = false;
   
   ngOnInit() {
     this.sets = this.getSets(0, [], this.axes, []);
-    this.priorities;
+
     for (let s = 0; s < this.sets.length; s += 1) {
       const sset = this.sets[s];
-      console.log('set:', sset);
-      
-      this.priorities.push(this.getPriorities(sset, 0));
+      const combos = this.getPermutations(sset as unknown as _.List<IObjValues>, sset.length);
+      // const combos = _.flatMap(sset, (v, i, a) => this.getPriorities(a as unknown as _.List<IObjValues>, i + 1));
+      this.priorities = this.priorities.concat(combos);
     }
   }
 
@@ -66,12 +67,12 @@ export class ModelAxesComponent implements OnInit {
     return initialCollection;
   }
 
-  getPriorities(collection: IObjValues, n: number): IObjValues {
-    let array = _.values(collection);
+  getCombinations(collection: _.List<IObjValues>, n: number): IObjValues {
+    let array = _.values(collection);    
     if (array.length < n) {
       return [];
     }
-    let recur = ((array: IObjValues, n: number) => {
+    let recur = ((array: any[], n: number) => {
       if (--n < 0) {
         return [[]];
       }
@@ -89,14 +90,26 @@ export class ModelAxesComponent implements OnInit {
     return recur(array, n);
   }
 
-  // getPriorities(index: number, initialCollection: IObjValues, set: IObjValues, previous: IObjValue): IObjValues {
-  //   if (index === set.length) {
-  //     initialCollection.push(previous);
-  //     return initialCollection;
-  //   }
-  //   for (let i = 0; i < set.length; i++) {
-  //     this.getPriorities(index + 1, initialCollection, set, set.slice(i, i + 1) );
-  //   }
-  //   return initialCollection;
-  // }
+  getPermutations(collection: _.List<IObjValues>, n: number) {
+    let array = _.values(collection);
+    if (array.length < n) {
+      return [];
+    }
+    let recur = ((array: any[], n: number) => {
+      if (--n < 0) {
+        return [[]];
+      }
+      let permutations: any[] = [];
+      array.forEach((value, index, array) => {
+        array = array.slice();
+        array.splice(index, 1);
+        recur(array, n).forEach(permutation => {
+          permutation.unshift(value);
+          permutations.push(permutation);
+        });
+      });
+      return permutations;
+    });
+    return recur(array, n);
+  }
 }
